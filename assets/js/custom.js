94393c237f605dda4c5fa2d77c41c5c99617bfaf
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2022-05-16 17:12:41 +0600

    another package

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,16 +10,21 @@ $(document).ready(function(){
         loop:true, 
         nav:true,
         dots:false, 
-        items:4,
         // autoplay: true,
         margin:10,
         navText: ["<i class='fa-solid fa-arrow-left'></i>","<i class='fa-solid fa-arrow-right'></i>"],
         responsive:{ 
             0:{ 
                 nav:false,
+                items:1,
             },
             600:{ 
                 nav:true,
+                items:2,
+            }  ,
+            900:{ 
+                nav:true,
+                items:4,
             } 
         }
     })
